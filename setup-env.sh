b9fe4227c82ffdfe2b0ebee6bd53f096e0c7606c
Hunter Hu <hunterhu.ca@gmail.com>
2014-10-29 12:06:53 -0500

    Add gpicview package

--- setup-env.sh
+++ setup-env.sh
@@ -32,7 +32,7 @@ echo "Installing essential packages..."
 echo "================================"
 pause
 PACKAGES=( cscope ctags git gitk openssh openssh-server tree meld vim chromium-browser ntp
-ack-grep htop qemu-user-static minicom sshpass )
+ack-grep htop qemu-user-static minicom sshpass gpicview )
 for i in "${PACKAGES[@]}"
 do
      echo "======================"
