b94fa95dc689d8270c6fac95bebe50f6de74f2d0
Hunter Hu <hunterhu.ca@gmail.com>
2015-03-20 17:06:10 -0500

    Add linux-tools-common package

--- setup-env.sh
+++ setup-env.sh
@@ -32,7 +32,7 @@ echo "Installing essential packages..."
 echo "================================"
 pause
 PACKAGES=( cscope ctags git gitk openssh openssh-server tree meld vim chromium-browser ntp
-ack-grep htop qemu-user-static minicom sshpass gpicview mousepad vim-gtk )
+ack-grep htop qemu-user-static minicom sshpass gpicview mousepad vim-gtk linux-tools-common )
 for i in "${PACKAGES[@]}"
 do
      echo "======================"
