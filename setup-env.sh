d137b04d807481b32babd4b0ba9abb8a38343271
Hunter Hu <hunterhu.ca@gmail.com>
2016-06-03 12:11:43 -0400

    Added bless to packages list
    bless is a raw image reader editor.

--- setup-env.sh
+++ setup-env.sh
@@ -45,7 +45,7 @@ echo "================================"
 echo "Installing essential packages..."
 echo "================================"
 pause
-PACKAGES=( cscope ctags git gitk openssh openssh-server tree meld vim ntp build-essential
+PACKAGES=( bless cscope ctags git gitk openssh openssh-server tree meld vim ntp build-essential
 ack-grep htop qemu-user-static minicom gpicview mousepad vim-gtk linux-tools-common inotify-tools libc6-dev-i386 )
 for i in "${PACKAGES[@]}"
 do
